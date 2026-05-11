20026083f9a0c47d6987a788d53a291e03fca23d
compscitwilight <compscitwilight@gmail.com>
2026-05-10 23:24:53 -0500

    add profileOptions.ts logic

--- src/profileOptions.ts
+++ src/profileOptions.ts
@@ -1,3 +1,9 @@
 // specifically for https://rateyourmusic.com/account/account_edit to detect site language modification 
 // see options.ts for actual preferences page configuration
 
+const languageSelection = document.querySelector<HTMLSelectElement>("select[name=\"language\"]");
+if (languageSelection) {
+    languageSelection.addEventListener("change", () => {
+        localStorage.setItem("_tyr_lang", languageSelection.value.toUpperCase());
+    })
+}
