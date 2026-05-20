6e21b03ab4dbb294af995b770e227a6dca048cd9
compscitwilight <compscitwilight@gmail.com>
2026-05-20 04:11:03 -0500

    finalize translation caching functionality

--- redis.ts
+++ redis.ts
@@ -1,4 +1,5 @@
 import { Redis } from "@upstash/redis";
+import { franc } from "franc";
 import * as crypto from "node:crypto";
 
 interface TrialInfo {
@@ -11,6 +12,8 @@ const client = new Redis({
     token: process.env.REDIS_TOKEN
 });
 
+const CACHED_REVIEW_EXPIRY: number = 60 * 60 * 24 * 30;
+
 export async function getTrialInfo(trialId: string): Promise<TrialInfo | null> {
     const count = await client.get<number>(`ext:${trialId}:count`);
     const firstSeen = await client.get<number>(`ext:${trialId}:firstSeen`);
@@ -21,8 +24,8 @@ export async function getTrialInfo(trialId: string): Promise<TrialInfo | null> {
 
 export async function registerTrial(): Promise<string> {
     const trialId = crypto.randomBytes(16).toString("hex");
-    await client.set(`ext:${trialId}:count`, 0);
-    await client.set(`ext:${trialId}:firstSeen`, Date.now());
+    await client.set<number>(`ext:${trialId}:count`, 0);
+    await client.set<number>(`ext:${trialId}:firstSeen`, Date.now());
     console.log(`created trial with ID ${trialId}`);
     return trialId;
 }
@@ -31,7 +34,20 @@ export async function billTrial(trialId: string, tokens: number) {
     return await client.incrby(`ext:${trialId}:count`, tokens);
 }
 
-export async function cacheReview(reviewText: string) {
-    const hash = crypto.createHash("md5").update(reviewText).digest().toString();
+export async function cacheReview(originalReview: string, translatedReview: string, targetLanguage: string) {
+    const hash = crypto.createHash("md5").update(`${originalReview}\x00${targetLanguage}`).digest("hex");
+    const sourceLanguage = franc(originalReview).slice(0, 2);
+    await client.set<string>(`review:${hash}`, translatedReview, { ex: CACHED_REVIEW_EXPIRY });
+    await client.set<string>(`review:${hash}:source_lang`, sourceLanguage, { ex: CACHED_REVIEW_EXPIRY });
+}
 
+export async function getCachedReview(originalReview: string, targetLanguage: string) {
+    const hash = crypto.createHash("md5").update(`${originalReview}\x00${targetLanguage}`).digest("hex");
+    const translatedReview = await client.get<string>(`review:${hash}`);
+    const sourceLanguage = await client.get<string>(`review:${hash}:source_lang`);
+    return (translatedReview !== null && sourceLanguage !== null) ? {
+        // mimics DeepL API response
+        text: translatedReview,
+        detected_source_language: sourceLanguage
+    } : null;
 }
